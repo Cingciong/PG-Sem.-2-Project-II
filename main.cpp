2e38c2e95a82052e76202edc5ed9944ad3b94641
Cingciong <kulakesikula@gmail.com>
2024-03-11 01:20:44 +0100

    accidentally making whole project 👉👈

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
+#include <queue>
 
 #define AMOUNT_OF_CARDS 52
 #define AMOUNT_OF_CARDS_IN_SUITE 13
@@ -33,7 +34,9 @@ string card_value[AMOUNT_OF_CARDS_IN_SUITE]{
 };
 
 
+
 class Card{
+public:
     int value;
     int suit;
 
@@ -41,7 +44,6 @@ public:
     void add_card(int given_value, int suit_given ){
         value = given_value;
         suit = suit_given;
-        cout<<card_value[value]<<", "<<card_suit[suit]<<"\n";
     }
 
     void display(){
@@ -52,6 +54,8 @@ public:
 class Deck {
 public:
     vector<Card> cards;
+    queue<Card> player_1;
+    queue<Card> player_2;
 
 public:
     void create() {
@@ -84,15 +88,79 @@ public:
         }
     }
 
+    void split(){
+        for (int i = 0; i < AMOUNT_OF_CARDS; ++i) {
+            if(AMOUNT_OF_CARDS/2>i){
+                player_1.push(cards[i]);
+            }else{
+                player_2.push(cards[i]);
+            }
+        }
+    }
+
+
 };
 
-
-
+vector <Card> war;
 
 
 int main() {
     Deck Deck;
     Deck.create();
     Deck.mix();
+    Deck.split();
 
+    while(1){
+        cout<<"player 1: "; Deck.player_1.front().display();
+        cout<<"player 2: ";Deck.player_2.front().display();
+
+        if(Deck.player_1.front().value>Deck.player_2.front().value){
+            cout<<"player 1 wins"<<"\n";
+
+
+            while (!war.empty()) {
+                Deck.player_1.push(war.back());
+                war.pop_back();
+            }
+
+
+            Deck.player_1.push(Deck.player_1.front()); // pushing to back of payer 1
+            Deck.player_1.push(Deck.player_2.front()); // pushing to back of payer 1
+
+            Deck.player_1.pop();
+            Deck.player_2.pop();
+        }
+        if(Deck.player_1.front().value<Deck.player_2.front().value){
+            cout<<"player 2 wins"<<"\n";
+
+            Deck.player_2.push(Deck.player_1.front()); // pushing to back of payer 2
+            Deck.player_2.push(Deck.player_2.front()); // pushing to back of payer 2
+
+            Deck.player_1.pop();
+            Deck.player_2.pop();
+        }
+        if(Deck.player_1.front().value==Deck.player_2.front().value){
+            cout<<"war!"<<"\n";
+
+            war.push_back(Deck.player_1.front()); // pushing to war
+            war.push_back(Deck.player_2.front()); // pushing to war
+            Deck.player_1.pop();
+            Deck.player_2.pop();
+            war.push_back(Deck.player_1.front()); // pushing to war
+            war.push_back(Deck.player_2.front()); // pushing to war
+        }
+
+        system("pause");
+        //end
+        if(Deck.player_2.empty()==1){
+            cout<<"player 1 wins!!! "<<"\n";
+            break;
+        }
+        if(Deck.player_1.empty()==1){
+            cout<<"player 2 wins!!! "<<"\n";
+            break;
+        }
+    }
 }
+
+
